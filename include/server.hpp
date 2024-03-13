6aa2345ee21df620b48283d2f7150145398127fa
Artyom-Gusarov <135129391+Artyom-Gusarov@users.noreply.github.com>
2024-03-13 17:42:03 +0300

    network for public chat realization

--- include/server.hpp
+++ include/server.hpp
@@ -7,50 +7,57 @@
 #include <queue>
 #include <string>
 #include <vector>
-
 #include "message_types.hpp"
 
 using boost::asio::ip::tcp;
 
 namespace meow::network {
 class Server {
-  boost::asio::io_context io_context;
-  tcp::acceptor acceptor;
-  mutable std::mutex mtx;
-  std::queue<Action> received_actions;
-  std::vector<std::thread> client_threads;
-  std::map<std::size_t, tcp::iostream*> client_streams;
-  std::vector<PlayerInfo> players_info;
-  std::condition_variable got_players_info;
-  void handle_client(tcp::socket&);
-  void send_players_info(std::size_t);
+    boost::asio::io_context io_context;
+    tcp::acceptor acceptor;
+    mutable std::mutex mtx;
+    std::queue<Action> received_actions;
+    std::vector<std::thread> client_threads;
+    std::map<std::size_t, tcp::iostream *> client_streams;
+    std::vector<PlayerInfo> players_info;
+    std::condition_variable got_players_info;
+    void handle_client(tcp::socket &);
+    void send_players_info(std::size_t);
 
- public:
-  Server();
+public:
+    Server();
 
-  ~Server();
+    ~Server();
 
-  // return port number for connection, get_id now is unresolved problem
-  [[nodiscard]] std::string get_port() const;
+    // return port number for connection, get_id now is unresolved problem
+    [[nodiscard]] std::string get_port() const;
 
-  // returns ids of existed clients
-  [[nodiscard]] std::vector<std::size_t> get_clients_id() const;
+    // returns ids of existed clients
+    [[nodiscard]] std::vector<std::size_t> get_clients_id() const;
 
-  // start waiting for clients in specified count, if count is wrong - now some
-  // problems(i work to fix it)
-  void start_listening(std::size_t num_of_clients);
+    // start waiting for clients in specified count, if count is wrong - now some
+    // problems(i work to fix it)
+    void start_listening(std::size_t num_of_clients);
 
-  // send to client with this id action
-  void send_action(std::size_t client_id, const Action& action);
+    // send to client with this id action
+    void send_action(std::size_t client_id, const Action &action);
 
-  // get action from que with id of client sent it
-  std::optional<Action> receive_action();
+    // get action from que with id of client sent it
+    std::optional<Action> receive_action();
 
-  // send ActionResult on client action, true - valid, false - invalid
-  void send_action_result(std::size_t client_id,
-                          const ActionResult& action_result);
+    // send ActionResult on client action, true - valid, false - invalid
+    void send_action_result(std::size_t client_id, const ActionResult &action_result);
 
-  void send_action_to_all_clients(const Action& action);
+    void send_action_to_all_clients(const Action &action);
+
+    void send_chat_message(std::size_t client_id, const ChatMessage &chat_message);
+
+    void send_chat_message_to_all_clients(const ChatMessage &chat_message);
 };
+
+Server& server_instance() {
+    static Server instance;
+    return instance;
+}
 }  // namespace meow::network
 #endif  // MEOWCHKIN_SERVER_HPP
--- src/client.cpp
+++ src/client.cpp
@@ -1,5 +1,4 @@
 #include "client.hpp"
-
 #include <boost/asio.hpp>
 #include <iostream>
 #include <nlohmann/json.hpp>
@@ -7,98 +6,127 @@
 using boost::asio::ip::tcp;
 
 namespace meow::network {
-Client::Client() : io_context() {}
+Client::Client() : io_context() {
+}
 
 Client::~Client() = default;
 
 void Client::accept_info_about_players() {
-  std::size_t count_of_players;
-  connection >> count_of_players;
-  connection >> id_of_client;
+    std::size_t count_of_players;
+    connection >> count_of_players;
+    connection >> id_of_client;
 
-  for (std::size_t i = 0; i < count_of_players; ++i) {
-    std::string input_msg;
-    while (input_msg.empty()) {
-      std::getline(connection, input_msg);
+    for (std::size_t i = 0; i < count_of_players; ++i) {
+        std::string input_msg;
+        while (input_msg.empty()) {
+            std::getline(connection, input_msg);
+        }
+        auto json = json::parse(input_msg);
+        players_info.emplace_back(json);
     }
-    auto json = json::parse(input_msg);
-    players_info.emplace_back(json);
-  }
 }
 
-void Client::connect(const std::string& host) {
-  size_t delimiter_pos = host.find(':');
-  std::string ip = host.substr(0, delimiter_pos);
-  std::string port = host.substr(delimiter_pos + 1);
+void Client::connect(const std::string &host) {
+    size_t delimiter_pos = host.find(':');
+    std::string ip = host.substr(0, delimiter_pos);
+    std::string port = host.substr(delimiter_pos + 1);
 
-  tcp::socket s(io_context);
-  boost::asio::connect(s, tcp::resolver(io_context).resolve(ip, port));
-  connection = tcp::iostream(std::move(s));
+    tcp::socket s(io_context);
+    boost::asio::connect(s, tcp::resolver(io_context).resolve(ip, port));
+    connection = tcp::iostream(std::move(s));
 
-  connection << name_of_client + "\n" << std::flush;
-  accept_info_about_players();
+    connection << name_of_client + "\n" << std::flush;
+    accept_info_about_players();
 
-  std::thread([this]() mutable {
-    while (connection) {
-      std::string received_msg;
-      if (!std::getline(connection, received_msg)) {
-        break;
-      }
-      if (received_msg.empty()) continue;
-      auto json = json::parse(received_msg);
-      std::unique_lock l(mtx);
-      if (json.contains("validness")) {
-        received_action_results.emplace(json);
-      } else {
-        received_actions.emplace(json);
-      }
-    }
-  }).detach();
+    std::thread([this]() mutable {
+        while (connection) {
+            std::string received_msg;
+            if (!std::getline(connection, received_msg)) {
+                break;
+            }
+            if (received_msg.empty()) {
+                continue;
+            }
+            auto json = json::parse(received_msg);
+            std::unique_lock l(mtx);
+            std::string received_type;
+            json.at("type").get_to(received_type);
+            if (received_type == "Action") {
+                received_actions.emplace(json);
+            }
+            if (received_type == "ActionResult") {
+                received_action_results.emplace(json);
+            }
+            if (received_type == "ChatMessage") {
+                received_chat_messages.emplace(json);
+            }
+        }
+    }).detach();
 }
 
-void Client::disconnect() { connection.close(); }
+void Client::disconnect() {
+    connection.close();
+}
 
 [[nodiscard]] std::size_t Client::get_id_of_client() const {
-  return id_of_client;
+    return id_of_client;
 }
 
 [[nodiscard]] std::string Client::get_name_of_client() const {
-  return name_of_client;
+    return name_of_client;
 }
 
-void Client::set_name_of_client(const std::string& name) {
-  name_of_client = name;
+void Client::set_name_of_client(const std::string &name) {
+    name_of_client = name;
 }
 
-[[nodiscard]] const std::vector<PlayerInfo>& Client::get_players_info() const {
-  return players_info;
+[[nodiscard]] const std::vector<PlayerInfo> &Client::get_players_info() const {
+    return players_info;
 }
 
-void Client::send_action(const Action& action) {
-  auto json = action.to_json();
-  std::string message_to_send;
-  message_to_send = json.dump();
-  message_to_send += "\n";
-  connection << message_to_send << std::flush;
+void Client::send_action(const Action &action) {
+    auto json = action.to_json();
+    std::string message_to_send;
+    message_to_send = json.dump();
+    message_to_send += "\n";
+    connection << message_to_send << std::flush;
 }
 
 std::optional<Action> Client::receive_action() {
-  std::unique_lock l(mtx);
-  if (received_actions.empty()) {
-    return std::nullopt;
-  }
-  Action action = received_actions.front();
-  received_actions.pop();
-  return action;
+    std::unique_lock l(mtx);
+    if (received_actions.empty()) {
+        return std::nullopt;
+    }
+    Action action = received_actions.front();
+    received_actions.pop();
+    return action;
 }
 
 std::optional<ActionResult> Client::receive_action_result() {
-  std::unique_lock l(mtx);
-  if (received_action_results.empty()) {
-    return std::nullopt;
-  }
-  ActionResult action_result = received_action_results.front();
-  received_action_results.pop();
-  return action_result;
+    std::unique_lock l(mtx);
+    if (received_action_results.empty()) {
+        return std::nullopt;
+    }
+    ActionResult action_result = received_action_results.front();
+    received_action_results.pop();
+    return action_result;
+}
+
+void Client::send_chat_message(const ChatMessage &chat_message) {
+    auto json = chat_message.to_json();
+    std::string message_to_send;
+    message_to_send = json.dump();
+    message_to_send += "\n";
+    connection << message_to_send << std::flush;
+}
+
+std::optional<ChatMessage> Client::receive_chat_message() {
+    std::unique_lock l(mtx);
+    if (received_chat_messages.empty()) {
+        return std::nullopt;
+    }
+    ChatMessage chat_message = received_chat_messages.front();
+    received_chat_messages.pop();
+    return chat_message;
 }
 }  // namespace meow::network
