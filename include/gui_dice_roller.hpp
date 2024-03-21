64b5353b607f9cf0694895a723cdcecd97c16dcd
pppppptttttt <pasha.kort@gmail.com>
2024-03-22 00:38:18 +0300

    networking, connection first steps. almost works

--- include/gui_dice_roller.hpp
+++ include/gui_dice_roller.hpp
@@ -15,7 +15,22 @@ template <typename T = int>
     requires std::is_integral<T>::value
 [[nodiscard]] T
 random_integer(T low = std::numeric_limits<T>::min(), T high = std::numeric_limits<T>::max()) {
+    static bool first_call = true;
     static thread_local std::mt19937 rd_generator;
+    if (first_call) {
+        std::mt19937::result_type s =
+            rd_generator() ^
+            ((std::mt19937::result_type)std::chrono::duration_cast<std::chrono::seconds>(
+                 std::chrono::system_clock::now().time_since_epoch()
+             )
+                 .count() +
+             (std::mt19937::result_type)std::chrono::duration_cast<std::chrono::microseconds>(
+                 std::chrono::high_resolution_clock::now().time_since_epoch()
+             )
+                 .count());
+        rd_generator.seed(s);
+        first_call = false;
+    }
     return std::uniform_int_distribution<T>(low, high)(rd_generator);
 }
 
