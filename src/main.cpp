ef03619fbedae40b80cae7c19c9c50e56ca5730c
pppppptttttt <pasha.kort@gmail.com>
2024-03-21 23:24:41 +0300

    OOP incident in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,54 +5,80 @@
 #include "raylib-cpp.hpp"
 #include "scene.hpp"
 
-int main() {
-    SetTraceLogLevel(LOG_WARNING);
+namespace meow {
+
+class Application {
+public:
+    enum Type { CLIENT, SERVER };
+
+private:
     static constexpr int window_width = 1920;
     static constexpr int window_height = 1080;
     static constexpr const char *window_title = "meow";
-    raylib::Window window(
-        window_width, window_height, window_title,
-        /*FLAG_WINDOW_RESIZABLE |*/ FLAG_MSAA_4X_HINT | FLAG_FULLSCREEN_MODE
-    );
 
-    // load scenes from 'plugins'
-    const meow::EnumArray<meow::SceneType, std::pair<const char *, const char *>> plugin_names{
-        {meow::SceneType::MAIN_MENU, {"mainmenu-scene", "main_menu"}},
-        {meow::SceneType::GAME, {"game-scene", "game_view"}},
-    };
-    meow::Plugin<meow::Scene> main_menu(plugin_names[meow::SceneType::MAIN_MENU]);
-    meow::Plugin<meow::Scene> game_view(plugin_names[meow::SceneType::GAME]);
-    main_menu->attach_window(&window);
-    game_view->attach_window(&window);
+    inline static const meow::EnumArray<meow::SceneType, std::pair<const char *, const char *>>
+        plugin_names{
+            {meow::SceneType::MAIN_MENU, {"mainmenu-scene", "main_menu"}},
+            {meow::SceneType::GAME, {"game-scene", "game_view"}},
+        };
 
-    SetExitKey(0);
-    window.SetTargetFPS(60);
+    raylib::Window m_window;
+    meow::Plugin<meow::Scene> m_main_menu;
+    meow::Plugin<meow::Scene> m_game_view;
+    std::unique_ptr<meow::SceneManager> m_scene_manager;
 
-    auto scene_manager = std::make_unique<meow::SceneManager>();
-    scene_manager->set_scene(meow::SceneType::MAIN_MENU, main_menu.get());
-    scene_manager->set_scene(meow::SceneType::GAME, game_view.get());
+public:
+    explicit Application([[maybe_unused]] Type t)
+        : m_window(
+              window_width,
+              window_height,
+              window_title,
+              FLAG_MSAA_4X_HINT | FLAG_FULLSCREEN_MODE
+          ),
+          m_main_menu(plugin_names[meow::SceneType::MAIN_MENU]),
+          m_game_view(plugin_names[meow::SceneType::GAME]),
+          m_scene_manager(std::make_unique<meow::SceneManager>()) {
+        m_main_menu->attach_window(&m_window);
+        m_game_view->attach_window(&m_window);
 
-    while (!window.ShouldClose() && scene_manager->active_scene()->running()) {
-        // application response
-        {
-            if (IsKeyDown(KEY_LEFT_SHIFT) && IsKeyPressed(KEY_R)) {
-                main_menu.reload(plugin_names[meow::SceneType::MAIN_MENU]);
-                main_menu->attach_window(&window);
-                scene_manager->set_scene(meow::SceneType::MAIN_MENU, main_menu.get());
-                game_view.reload(plugin_names[meow::SceneType::GAME]);
-                game_view->attach_window(&window);
-                scene_manager->set_scene(meow::SceneType::GAME, game_view.get());
-            }
-        }
-        // application render
-        window.BeginDrawing();
-        {
-            // if (IsKeyPressed(KEY_R)) {
-            //     meow::play_dice_animation(&window, -4, 8);
-            // }
-            window.ClearBackground(raylib::Color::RayWhite());
-            scene_manager->draw_scene();
-        }
-        window.EndDrawing();
+        m_scene_manager->set_scene(meow::SceneType::MAIN_MENU, m_main_menu.get());
+        m_scene_manager->set_scene(meow::SceneType::GAME, m_game_view.get());
+
+        SetExitKey(0);
+        m_window.SetTargetFPS(60);
     }
+
+    void run() {
+        while (!m_window.ShouldClose() && m_scene_manager->active_scene()->running()) {
+            response();
+            render();
+        }
+    }
+
+private:
+    void response() {
+        if (IsKeyDown(KEY_LEFT_SHIFT) && IsKeyPressed(KEY_R)) {
+            m_main_menu.reload(plugin_names[meow::SceneType::MAIN_MENU]);
+            m_main_menu->attach_window(&m_window);
+            m_scene_manager->set_scene(meow::SceneType::MAIN_MENU, m_main_menu.get());
+            m_game_view.reload(plugin_names[meow::SceneType::GAME]);
+            m_game_view->attach_window(&m_window);
+            m_scene_manager->set_scene(meow::SceneType::GAME, m_game_view.get());
+        }
+    }
+
+    void render() {
+        m_window.BeginDrawing();
+        m_window.ClearBackground(raylib::Color::RayWhite());
+        m_scene_manager->draw_scene();
+        m_window.EndDrawing();
+    }
+};
+
+}  // namespace meow
+
+int main() {
+    SetTraceLogLevel(LOG_WARNING);
+    auto app = std::make_unique<meow::Application>(meow::Application::CLIENT);
+    app->run();
 }
